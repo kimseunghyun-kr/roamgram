68321702a14229f641870e87271d821829b9d49f
kimseunghyun-kr <incongnito12@gmail.com>
2024-05-14 02:49:03 +0900

    changed to official aws sdk

---
+++ src/main/java/com/example/travelDiary/application/S3/S3Config.java
@@ -0,0 +1,34 @@
+package com.example.travelDiary.application.S3;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+class S3Config {
+    @Value("${aws.s3.access-key}")
+    private String accessKey;
+
+    @Value("${aws.s3.secret-key}")
+    private String secretKey;
+
+    @Value("${aws.s3.region.static}")
+    private String region;
+
+    @Bean
+    public AmazonS3 amazonS3Client(){
+        return AmazonS3ClientBuilder.standard()
+                .withCredentials(
+                        new AWSStaticCredentialsProvider(
+                                new BasicAWSCredentials(accessKey, secretKey)
+                        )
+                )
+                .withRegion(Regions.valueOf(region))
+                .build();
+    }
+}

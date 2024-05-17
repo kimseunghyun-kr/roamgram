2b7139b1d669ebbcedb2dea1d1baeb9dfdbf3e5e
kimseunghyun-kr <incongnito12@gmail.com>
2024-05-18 03:07:34 +0900

    major feat: linked amazon lambda to callback upon actual initiation of the upload. todo: make this work for delete

---
+++ src/main/java/com/example/travelDiary/presentation/dto/request/s3/lambda/LambdaUploadCompleteRequest.java
@@ -0,0 +1,10 @@
+package com.example.travelDiary.presentation.dto.request.s3.lambda;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
+public class LambdaUploadCompleteRequest {
+    @JsonProperty("objectKey")
+    public String objectKey;
+}

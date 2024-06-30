4a40dd2889eb503402d53cb96d6df578be28ccf1
kimseunghyun-kr <incongnito12@gmail.com>
2024-06-30 23:29:23 +0900

    adding access checks and inject resources for the get methods associated with Review

--- src/main/java/com/example/travelDiary/application/service/review/ReviewAccessService.java
+++ src/main/java/com/example/travelDiary/application/service/review/ReviewAccessService.java
@@ -1,36 +1,47 @@
 package com.example.travelDiary.application.service.review;
 
+import com.example.travelDiary.common.permissions.aop.CheckAccess;
+import com.example.travelDiary.common.permissions.aop.InjectResourceIds;
+import com.example.travelDiary.common.permissions.domain.UserResourcePermissionTypes;
 import com.example.travelDiary.domain.model.review.Review;
+import com.example.travelDiary.domain.model.travel.Schedule;
 import com.example.travelDiary.repository.persistence.review.ReviewRepository;
+import org.hibernate.annotations.Check;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.convert.ConversionService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
 public class ReviewAccessService {
-    private final MediaFileAccessService mediaFileAccessService;
-    private final ConversionService conversionService;
+
     private final ReviewRepository reviewRepository;
 
     @Autowired
-    public ReviewAccessService(MediaFileAccessService mediaFileAccessService, ConversionService conversionService, ReviewRepository reviewRepository) {
-        this.mediaFileAccessService = mediaFileAccessService;
-        this.conversionService = conversionService;
+    public ReviewAccessService(ReviewRepository reviewRepository) {
         this.reviewRepository = reviewRepository;
     }
 
-    public Review getReviewById(UUID id) {
-        return reviewRepository.findById(id).orElseThrow();
+    @CheckAccess(resourceType = Schedule.class, spelResourceId = "#reviewId", permission = "EDIT")
+    public Review getReviewById(UUID reviewId) {
+        return reviewRepository.findById(reviewId).orElseThrow();
     }
 
+    @CheckAccess(resourceType = Schedule.class, spelResourceId = "#scheduleId", permission = "EDIT")
     public Page<Review> getAllReviewsFromSchedule(UUID ScheduleId, Integer page, Integer size) {
         Pageable pageable = PageRequest.of(page, size);
         return reviewRepository.findAllByScheduleId(ScheduleId, pageable);
+    }
+
+    @InjectResourceIds(parameterName = "resourceIds", resourceType = "Review", permissionType = UserResourcePermissionTypes.VIEW)
+    public Page<Review> getAllAuthorisedReviews(List<UUID> resourceIds, Integer page, Integer size) {
+        Pageable pageable = PageRequest.of(page, size);
+        Page<Review> result = reviewRepository.findAllAuthorized(resourceIds, pageable);
+        return result;
     }
 
 }
--- src/main/java/com/example/travelDiary/repository/persistence/review/ReviewRepository.java
+++ src/main/java/com/example/travelDiary/repository/persistence/review/ReviewRepository.java
@@ -4,10 +4,15 @@ import com.example.travelDiary.domain.model.review.Review;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface ReviewRepository extends JpaRepository<Review, UUID> {
     Page<Review> findAllByScheduleId(UUID scheduleId, Pageable pageable);
+
+    @Query("SELECT r FROM Review r" +
+            " WHERE r.resource.id IN :resourceIds")
+    Page<Review> findAllAuthorized(List<UUID> resourceIds, Pageable pageable);
 }

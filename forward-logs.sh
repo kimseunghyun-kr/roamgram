bba0592c520126a073beff45bb5224f2a2f8c881
kimseunghyun-kr <incongnito12@gmail.com>
2024-07-08 02:57:23 +0900

    modified logback for more effective logs being captured and logstash to accomodate for. cron job - ran forward-logs are modified so that the materials inside the original log files are cleared every cron run

--- forward-logs.sh
+++ forward-logs.sh
@@ -6,10 +6,23 @@ LOG_FILE="/var/lib/docker/volumes/travelplanner_spring-boot-logs/_data/roamgram.
 # Define the destination (host and port) where the log should be forwarded
 DESTINATION="localhost:5044"
 
+# Define a temporary file to store logs before sending
+TEMP_FILE="/tmp/temp_roamgram.log"
+
 # Check if the log file exists
 if [ -f "$LOG_FILE" ]; then
-  # Use tail to continuously forward the logs to the destination
-  tail -F "$LOG_FILE" | nc -q0 $(echo $DESTINATION | tr ':' ' ')
+  # Move the log content to a temporary file and clear the original log file
+  mv "$LOG_FILE" "$TEMP_FILE"
+  : > "$LOG_FILE"
+
+  # Check if the temporary file is not empty
+  if [ -s "$TEMP_FILE" ]; then
+    # Send the logs to the destination
+    cat "$TEMP_FILE" | nc -q0 $(echo $DESTINATION | tr ':' ' ')
+
+    # Remove the temporary file after sending
+    rm "$TEMP_FILE"
+  fi
 else
   echo "Log file not found: $LOG_FILE"
   exit 1
